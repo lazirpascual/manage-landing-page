1f66647a9c092cc382309a63bdfa4b91ddbe252d
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-01-09 01:50:33 -0500

    fix navbar and make it responsive

--- src/components/MainNavigation/MainNavigation.jsx
+++ src/components/MainNavigation/MainNavigation.jsx
@@ -1,31 +1,65 @@
+import { useState } from 'react';
+import './MainNavigation.css';
+
 export default function MainNavigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigationClass = isMenuOpen
+    ? 'primary-navigation-visible'
+    : 'primary-navigation';
+  const headerClass = isMenuOpen
+    ? 'primary-header-data-overlay'
+    : 'primary-header';
+  const ariaExpanded = isMenuOpen ? 'true' : 'false';
+
   return (
     <>
-      <header className="primary-header">
+      <header className={headerClass}>
         <div className="container">
-          <a href="#" className="logo">
-            <img src="images/logo.svg" alt="Manage"></img>
-          </a>
-          <nav className="primary-navigation">
-            <ul aria-label="Primary" role="list" className="nav-list">
-              <li>
-                <a href="#">Pricing</a>
-              </li>
-              <li>
-                <a href="#">Product</a>
-              </li>
-              <li>
-                <a href="#">About Us</a>
-              </li>
-              <li>
-                <a href="#">Careers</a>
-              </li>
-              <li>
-                <a href="#">Community</a>
-              </li>
-            </ul>
-          </nav>
-          <button className="button">Get Started</button>
+          <div className="nav-wrapper">
+            <a href="#" className="logo">
+              <img src="images/logo.svg" alt="Manage"></img>
+            </a>
+            <button
+              className="mobile-nav-toggle"
+              aria-controls="primary-navigation"
+              aria-expanded={ariaExpanded}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <img
+                className="icon-hamburger"
+                src={
+                  isMenuOpen
+                    ? 'images/icon-close.svg'
+                    : 'images/icon-hamburger.svg'
+                }
+                alt=""
+                aria-hidden="true"
+              />
+              <span className="visually-hidden">Menu</span>
+            </button>
+            <nav className={navigationClass} id="primary-navigation">
+              <ul aria-label="Primary" role="list" className="nav-list" i>
+                <li>
+                  <a href="#">Pricing</a>
+                </li>
+                <li>
+                  <a href="#">Product</a>
+                </li>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Careers</a>
+                </li>
+                <li>
+                  <a href="#">Community</a>
+                </li>
+              </ul>
+            </nav>
+            <button className="button | display-sm-none display-md-inline-flex">
+              Get Started
+            </button>
+          </div>
         </div>
       </header>
     </>
