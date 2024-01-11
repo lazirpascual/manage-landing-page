c0b3c765db73a6c83d811a78c3879f097711cbdf
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-01-11 01:39:19 -0500

    Fix ManageCTA styling

--- src/components/MainBody/components/ManageCTA/ManageCTA.jsx
+++ src/components/MainBody/components/ManageCTA/ManageCTA.jsx
@@ -1,15 +1,16 @@
+import './ManageCTA.css';
 export default function ManageCTA() {
   return (
     <>
       <section className="cta | padding-block-900 bg-accent-400 text-neutral-100">
         <div className="container">
-          <div className="even-columns">
+          <div className="even-columns vertical-align-center">
             <div>
-              <p className="fs-secondary-heading fw-bold">
+              <p className="fs-primary-heading fw-bold">
                 Simplify how your team works today.
               </p>
             </div>
-            <div>
+            <div className="justify-self-end-md">
               <button className="button" data-type="inverted">
                 Get Started
               </button>
